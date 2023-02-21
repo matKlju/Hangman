2aa9495b6191d7a4372d148a00bd7231bb2ed008
matKlju <108662553+matKlju@users.noreply.github.com>
2023-02-21 14:25:36 +0200

    update
    Total rework of hangman
    ascii hanging
    random word from net

--- Hangman.py
+++ Hangman.py
@@ -1,66 +1,131 @@
 """
 
-Hangman
-
-To-do:
-    Add pictures of hanging.
+Hangman game
 
 """
 
-import PySimpleGUI as sg
+from sys import exit as sys_exit
 from random import choice as rnd
-from words import words
+import urllib.request
 
-guessed_word = []
+WORD_SITE = "https://www.mit.edu/~ecprice/wordlist.10000"
 
-guessed_letters = []
+with urllib.request.urlopen(WORD_SITE) as f:
+    data = f.read()
 
-word = [i for i in rnd(words) for y in i]  # random word to guess
+WORDS = tuple(str(i)[2:-1] for i in data.splitlines() if len(str(i)[2:-1]) > 3)
 
-for i in range(len(word)):
-    guessed_word.append('__')  # word interface. '__' as guessed letters
 
-tries_left = len(word) * 2  # tries left
+def display_noose(value):
+    """Display hanging stage depending tries"""
 
-while '__' in guessed_word:  # loop keeps going while it still has '__'
-    
-    # State of game, guess letter
-    layout = [
-        [sg.Text(
-            f'Guessed letters: {guessed_letters}\ncount: {len(guessed_letters)}\n\n'
-            f'Tries left: {tries_left}\nWord: {guessed_word}\nGuess one letter:')],
-        [sg.InputText()],
-        [sg.Ok()]
-    ]
+    if value == 7:
+        print('   _______\n  ||      |\n  ||      |\n'
+              '  ||\n  ||\n  ||\n /||\\\n/_||_\\\n')
 
-    window = sg.Window('Hangman', layout)
-    value = window.read()
-    window.close()
+    elif value == 6:
+        print('   _______\n  ||      |\n  ||      |\n'
+              '  ||      0\n  ||\n  ||\n /||\\\n/_||_\\\n')
 
-    guess = (value[1])[0]
+    elif value == 5:
+        print('   _______\n  ||      |\n  ||      |\n'
+              '  ||      0\n  ||      |\n  ||\n /||\\\n/_||_\\\n')
 
-    # If tries over = game over
-    if tries_left == 0:
-        sg.popup(f'\nYou LOST! Out of tries.\nThe word was: {word}')
-        exit()
+    elif value == 4:
+        print('   _______\n  ||      |\n  ||      |\n'
+              '  ||      0\n  ||     /|\n  ||\n /||\\\n/_||_\\\n')
 
-    if len(guess) > 1:    # if more than one letter, restart loop
-        sg.popup('Too long! One letter only!')
-        
-    elif guess in word:   # if letter occurs in word, start check
-        index_count = -1
-        
-        for i in word:    # iterate through word, check for letters
-            index_count += 1
-            
-            if str(i) == guess:    # index_count to print reoccurring letters
-                guessed_word[index_count] = guess
-    
-    else:    # when wrong letter then else block
-        sg.popup('No such letter in word')
-        guessed_letters.append(guess)
-        tries_left -= 1
-    
-    # if no '__' in guessed_word, loop ends and game won or lost!
+    elif value == 3:
+        print('   _______\n  ||      |\n  ||      |\n'
+              '  ||      0\n  ||     /|\\\n  ||\n /||\\\n/_||_\\\n')
 
-sg.popup(f'\nYou WON!\nWord: {guessed_word}')
+    elif value == 2:
+        print('   _______\n  ||      |\n  ||      |\n'
+              '  ||      0\n  ||     /|\\\n  ||      |\n /||\\\n/_||_\\\n')
+
+    elif value == 1:
+        print('   _______\n  ||      |\n  ||      |\n'
+              '  ||      0\n  ||     /|\\\n  ||      |\n /||\\    /\n/_||_\\\n')
+
+    elif value == 0:
+        print('   _______\n  ||      |\n  ||      |\n'
+              '  ||      0\n  ||     /|\\\n  ||      |\n /||\\    / \\\n/_||_\\\n')
+
+
+def display_status(limit, wrong_letters, hidden_word):
+    """Display current game status"""
+
+    print(f'Tries left: {limit}\nWrong letters: {wrong_letters}\n'
+          f'Guess the word: {hidden_word}')
+
+
+def guess_word(word_to_guess):
+    """Let user guess word. Evaluate and return result"""
+
+    hidden_word = ['_' for _ in word_to_guess]
+    wrong_letters = []
+    limit = 7
+
+    while True:
+        try:
+            display_noose(limit)
+            display_status(limit, wrong_letters, hidden_word)
+            letter = input('\tLetter: ')
+
+        except ValueError:
+            print('Only letters!')
+            continue
+
+        if letter not in word_to_guess:
+            limit -= 1
+            wrong_letters.append(letter)
+            print('\nNo such letter in word\n')
+
+            if limit < 1:
+                display_noose(limit)
+                print(f'Out of tries!\n\nThe word was "{word_to_guess}"\n')
+                return False
+            continue
+
+        for index, value in enumerate(list(word_to_guess)):
+            if value == letter:
+                hidden_word.pop(index)
+                hidden_word.insert(index, letter)
+
+        if hidden_word == list(word_to_guess):
+            return True
+
+
+def game():
+    """Main game"""
+
+    word_to_guess = rnd(WORDS)
+
+    if guess_word(word_to_guess):
+        print('\nYou win!\n')
+    else:
+        print("\nYou have lost! Now your feet don't touch the ground\n")
+
+
+def main():
+    """Main menu 1 start game, 2 quit"""
+
+    while True:
+        try:
+            choice = int(input('(1) Play HANGMAN\n(2) Exit\n\t: '))
+        except ValueError:
+            print('Only number values! ')
+            continue
+
+        if choice < 1 or choice > 2:
+            print('Only choose 1 or 2')
+            continue
+        if choice == 1:
+            game()
+        else:
+            print('Bye!')
+            sys_exit()
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++
@@ -1,3 +0,0 @@
-# Hangman
-A simple hangman game witha basic view from terminal.
-Develope simple GUI
